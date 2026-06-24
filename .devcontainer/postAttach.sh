20ac91a59374e909b6672ba9cd3e1c9dac223206
Kegan Maher <kegan@compiler.la>
2026-06-24 00:44:28 +0000

    refactor(devcontainer): move node/npm install to Docker build
    for improved caching between container restarts

--- .devcontainer/postAttach.sh
+++ .devcontainer/postAttach.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 set -eu
 
-git config --global --add safe.directory /home/compiler/site
-
 # initialize hook environments
 pre-commit install --install-hooks --overwrite
 
 # manage commit-msg hooks
 pre-commit install --hook-type commit-msg
-
-# this triggers a node.js install using version specified in mise.toml
-mise exec -- npm ci

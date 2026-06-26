5ed8974a4175add393fc338821fe15a08186b12e
Kegan Maher <kegan@compiler.la>
2026-06-26 20:19:05 +0000

    refactor: simplify site data and template
    Events was already the default here, no need for site data and if statements

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,7 +2,6 @@ export default {
   title: "Data + Donuts LA",
   description: "Changing local government from the ground up.",
   email: "data-donuts@compiler.la",
-  event_label: "Events",
   github: "compilerla/data-donuts",
   timezone: "America/Los_Angeles",
   url: "https://datadonuts.la",
